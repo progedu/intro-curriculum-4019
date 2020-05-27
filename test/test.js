ed83c228c9a84f993fb5f066f946679458beb2d4
melissa <e-panjaitan@xware.co.jp>
2020-05-27 15:44:02 +0900

    test assert

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+const assert = require('assert');
+
 const User = require('../models/user');
 const Schedule = require('../models/schedule');
 const Candidate = require('../models/candidate');
@@ -105,7 +107,14 @@ describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
               .post(`/schedules/${scheduleId}/users/${0}/candidates/${candidate.candidateId}`)
               .send({ availability: 2 }) // 出席に更新
               .expect('{"status":"OK","availability":2}')
-              .end((err, res) => { deleteScheduleAggregate(scheduleId, done, err); });
+              .end((err, res) => {
+                Availability.findAll({
+                  where: { scheduleId: scheduleId }
+                }).then((availabilities) => {
+                  // TODO ここにテストを記述する
+                  deleteScheduleAggregate(scheduleId, done, err);
+                });
+              });
           });
         });
     });

bb2650246e531ead4e64f055f3fe9d7880282407
katuo-sakana <katuo.webkaihatsu@gmail.com>
2020-08-14 22:00:22 +0900

    データベースに存在するかどうかのテスト

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ const User = require('../models/user');
 const Schedule = require('../models/schedule');
 const Candidate = require('../models/candidate');
 const Availability = require('../models/availability');
+const assert = require('assert');
 
 describe('/login', () => {
   before(() => {
@@ -36,10 +37,7 @@ describe('/login', () => {
 
 describe('/logout', () => {
   it('/ にリダイレクトされる', (done) => {
-    request(app)
-      .get('/logout')
-      .expect('Location', '/')
-      .expect(302, done);
+    request(app).get('/logout').expect('Location', '/').expect(302, done);
   });
 });
 
@@ -58,7 +56,11 @@ describe('/schedules', () => {
     User.upsert({ userId: 0, username: 'testuser' }).then(() => {
       request(app)
         .post('/schedules')
-        .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
+        .send({
+          scheduleName: 'テスト予定1',
+          memo: 'テストメモ1\r\nテストメモ2',
+          candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3',
+        })
         .expect('Location', /schedules/)
         .expect(302)
         .end((err, res) => {
@@ -72,7 +74,13 @@ describe('/schedules', () => {
             .expect(/テスト候補2/)
             .expect(/テスト候補3/)
             .expect(200)
-            .end((err, res) => { deleteScheduleAggregate(createdSchedulePath.split('/schedules/')[1], done, err); });
+            .end((err, res) => {
+              deleteScheduleAggregate(
+                createdSchedulePath.split('/schedules/')[1],
+                done,
+                err
+              );
+            });
         });
     });
   });
@@ -93,19 +101,36 @@ describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
     User.upsert({ userId: 0, username: 'testuser' }).then(() => {
       request(app)
         .post('/schedules')
-        .send({ scheduleName: 'テスト出欠更新予定1', memo: 'テスト出欠更新メモ1', candidates: 'テスト出欠更新候補1' })
+        .send({
+          scheduleName: 'テスト出欠更新予定1',
+          memo: 'テスト出欠更新メモ1',
+          candidates: 'テスト出欠更新候補1',
+        })
         .end((err, res) => {
           const createdSchedulePath = res.headers.location;
           const scheduleId = createdSchedulePath.split('/schedules/')[1];
           Candidate.findOne({
-            where: { scheduleId: scheduleId }
+            where: { scheduleId: scheduleId },
           }).then((candidate) => {
             // 更新がされることをテスト
             request(app)
-              .post(`/schedules/${scheduleId}/users/${0}/candidates/${candidate.candidateId}`)
+              .post(
+                `/schedules/${scheduleId}/users/${0}/candidates/${
+                  candidate.candidateId
+                }`
+              )
               .send({ availability: 2 }) // 出席に更新
               .expect('{"status":"OK","availability":2}')
-              .end((err, res) => { deleteScheduleAggregate(scheduleId, done, err); });
+              .end((err, res) => {
+                Availability.findAll({
+                  where: { scheduleId: scheduleId },
+                }).then((availabilities) => {
+                  // TODO ここにテストを記述する
+                  assert.equal(availabilities.length, 1);
+                  assert.equal(availabilities[0].availability, 2);
+                  deleteScheduleAggregate(scheduleId, done, err);
+                });
+              });
           });
         });
     });
@@ -114,19 +139,23 @@ describe('/schedules/:scheduleId/users/:userId/candidates/:candidateId', () => {
 
 function deleteScheduleAggregate(scheduleId, done, err) {
   Availability.findAll({
-    where: { scheduleId: scheduleId }
+    where: { scheduleId: scheduleId },
   }).then((availabilities) => {
-    const promises = availabilities.map((a) => { return a.destroy(); });
+    const promises = availabilities.map((a) => {
+      return a.destroy();
+    });
     Promise.all(promises).then(() => {
       Candidate.findAll({
-        where: { scheduleId: scheduleId }
+        where: { scheduleId: scheduleId },
       }).then((candidates) => {
-        const promises = candidates.map((c) => { return c.destroy(); });
+        const promises = candidates.map((c) => {
+          return c.destroy();
+        });
         Promise.all(promises).then(() => {
-          Schedule.findByPk(scheduleId).then((s) => { 
-            s.destroy().then(() => { 
+          Schedule.findByPk(scheduleId).then((s) => {
+            s.destroy().then(() => {
               if (err) return done(err);
-              done(); 
+              done();
             });
           });
         });
